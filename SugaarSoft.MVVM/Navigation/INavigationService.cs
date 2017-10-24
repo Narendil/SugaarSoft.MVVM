e5304d3dc9107b5ee80868043b07b93d98933a2a
asier.barato <asier.barato@solmicro.com>
2017-10-24 13:12:44 +0200

    Added navigation contract

---
+++ SugaarSoft.MVVM/Navigation/INavigationService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace SugaarSoft.MVVM.Navigation
+{
+    interface INavigationService
+    {
+        Task NavigateTo<T>();
+        Task NavigateBack();
+    }
+}
